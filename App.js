43594ae23032ad1e5bc741d8cdb1117a3a0e5adb
Bruno Targa <127400761+targax@users.noreply.github.com>
2023-11-13 22:07:33 -0300

    Modo de entrada

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const Stack = createStackNavigator();
   export default function App() {   
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Pesquisa">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="Home"  component={Home}/>
         <Stack.Screen name="Setor"  component={SetorSocial}/>
